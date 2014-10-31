0326f0e5029a6d1d289bb648770da9f32b3811fb
Marcelo Boeira <me@marceloboeira.com>
2014-10-31 08:57:14 -0200

    Just commenting out...

--- firmware/uno.cpp
+++ firmware/uno.cpp
@@ -1,16 +1,26 @@
-#define AN_BAUD 9600
-#define AN_HANDSHAKE_IN "AN:HELLO_ARDUINO"
+#define AN_BAUD          9600
+#define AN_HANDSHAKE_IN  "AN:HELLO_ARDUINO"
 #define AN_HANDSHAKE_OUT "AN:HELLO_NODE"
-#define AN_NL '\n'
+#define AN_NL            '\n'
 
+
+/**
+ * Setup Arnode Stuff
+ */
 void setup() {
+  // Open the serial port
   Serial.begin(AN_BAUD);
+  // Wait for the hand-shake, before anything else
   _waitForHandShake();
+
+  // test stuff
   pinMode(13, OUTPUT);
   digitalWrite(13, LOW);
 }
 
 void loop(){
+
+  // Scratch part, will be removed soon, now we need it for tests
   while (Serial.available() > 0) {
     int inByte = Serial.read();
     Serial.println(inByte);
@@ -25,6 +35,9 @@ void loop(){
   }
 }
 
+/**
+ * Private 'method' to wait until the hand-shake is done :)
+ */
 void _waitForHandShake(){
   bool handShaked = false;
   while (!handShaked) {
@@ -35,5 +48,3 @@ void _waitForHandShake(){
     }
   }
 }
-
-
